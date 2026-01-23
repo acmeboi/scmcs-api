8f5d0bda75f9fdc03fedfc3a2e12d6841a6542b7
Isa Muhammed Alhaji <isamuhammad0131@gmail.com>
2026-01-23 14:48:05 +0100

    Replace custom polyfill with official symfony/polyfill-php84
    - Remove custom polyfill file (src/polyfill.php)
    - Remove polyfill from composer.json autoload files
    - Remove manual polyfill requires from public/index.php and bin/console
    - Install official symfony/polyfill-php84 package (^1.33)
    - Remove symfony/polyfill-php84 from composer.json replace section to allow installation
    - Official polyfill is automatically loaded via Composer autoload
    - Provides array_any() and other PHP 8.4 functions for PHP 8.2+ compatibility

--- public/index.php
+++ public/index.php
@@ -2,9 +2,6 @@
 
 use App\Kernel;
 
-// Load polyfill for PHP 8.4 functions (array_any) for PHP 8.2 compatibility
-require_once dirname(__DIR__).'/src/polyfill.php';
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
